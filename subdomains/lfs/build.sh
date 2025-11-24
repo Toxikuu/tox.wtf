222ba38abc5b55db7c8338837e213f45fd91d206
tox <tox@tox.wtf>
2025-11-24 15:05:48 -0600

    fix(lfs): hack around blfs failure

--- subdomains/lfs/build.sh
+++ subdomains/lfs/build.sh
@@ -7,7 +7,8 @@ for book in lfs slfs glfs blfs; do
     make clean
     for rev in sysv systemd; do
         echo "Building $book-$rev"
-        make REV=$rev THEMEDIR=../themes/themes THEME=sunset
+        # FIXME: Building BLFS with AUTO_CLEAN=1 is broken
+        make REV=$rev THEMEDIR=../themes/themes THEME=sunset AUTO_CLEAN=0
         make REV=$rev INSTALLROOT=../../../target/subdomains/lfs install
     done
 
