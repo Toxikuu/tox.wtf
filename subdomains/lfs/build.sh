c16a00b770ae9d4d22922bf70a360790be796630
tox <tox@tox.wtf>
2025-12-20 21:12:58 -0600

    fix(lfs): revert parallel builds
    The poor machine I'm running this on can't handle building the books in
    parallel, and weird race conditions also exist.

--- subdomains/lfs/build.sh
+++ subdomains/lfs/build.sh
@@ -12,17 +12,6 @@ for book in lfs slfs glfs blfs; do
             echo "Building $book-$rev"
             make REV=$rev THEMEDIR=../themes/themes THEME=sunset AUTO_CLEAN=0
             make REV=$rev INSTALLROOT=../../../target/subdomains/lfs install
-        ) &
-
-        jobs+=($!)
+        )
     done
 done
-
-status=0
-for pid in "${jobs[@]}"; do
-    if ! wait "$pid"; then
-        status=1
-    fi
-done
-
-exit "$status"
